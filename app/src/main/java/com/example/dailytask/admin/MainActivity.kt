6bf8ee59703a15bab477020ad33e33ca12477e17
VCX438 <jiawen.teoh@motorolasolutions.com>
2023-11-24 10:11:00 +0800

    Update on UI

--- app/src/main/java/com/example/dailytask/admin/MainActivity.kt
+++ app/src/main/java/com/example/dailytask/admin/MainActivity.kt
@@ -2,9 +2,11 @@ package com.example.dailytask.admin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.inputmethod.InputBinding
 import com.example.dailytask.R
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: InputBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
