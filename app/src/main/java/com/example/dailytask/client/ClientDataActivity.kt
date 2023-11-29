94f0a7838babb77d0f3f6e838231a41370c755b0
Shuang Jing Tay (Partner) <VND734@motorolasolutions.com>
2023-11-29 10:57:05 +0800

    update the client

---
+++ app/src/main/java/com/example/dailytask/client/ClientDataActivity.kt
@@ -0,0 +1,49 @@
+package com.example.dailytask.client
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import androidx.lifecycle.ViewModelProvider
+import androidx.room.Room
+import com.example.dailytask.databinding.ActivitySecondBinding
+import com.example.dailytask.db.Task
+import com.example.dailytask.db.TaskDatabase
+import com.example.dailytask.db.TaskRepository
+
+class ClientDataActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySecondBinding
+    private lateinit var clientTaskViewModel: ClientTaskViewModel
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySecondBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val database = Room.databaseBuilder(applicationContext, TaskDatabase::class.java, "task_database").build()
+        val repository = TaskRepository(database.taskDao)
+        clientTaskViewModel = ViewModelProvider(this, ClientTaskViewModelFactory(repository)).get(ClientTaskViewModel::class.java)
+
+        binding.btSave.setOnClickListener {
+            save()
+        }
+    }
+
+    private fun save() {
+        binding.apply {
+            if (!etTitle.text.isEmpty() && !etContent.text.isEmpty() && !etDate.text.isEmpty() && !etName.text.isEmpty()){
+                val userInputTitle = etTitle.text.toString()
+                val userInputContent = etContent.text.toString()
+                val userInputDate = etDate.text.toString()
+                val userInputName = etName.text.toString()
+                clientTaskViewModel.insert(Task(null, userInputTitle, userInputContent, userInputDate, userInputName, false))
+                etTitle.text.clear()
+                etContent.text.clear()
+                etDate.text.clear()
+                etName.text.clear()
+                finish()
+            } else {
+                Toast.makeText(applicationContext, "Please fill in the empty field.", Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+}
--- app/src/main/java/com/example/dailytask/client/ClientTaskViewModel.kt
+++ app/src/main/java/com/example/dailytask/client/ClientTaskViewModel.kt
@@ -1,19 +1,19 @@
 package com.example.dailytask.client
 
-import android.widget.Toast
+import android.util.Log
 import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.dailytask.db.Task
 import com.example.dailytask.db.TaskRepository
 import kotlinx.coroutines.launch
 
-class TaskViewModel(private val repository: TaskRepository): ViewModel() {
+class ClientTaskViewModel(private val repository: TaskRepository): ViewModel() {
 
     val allTasks: LiveData<List<Task>> = repository.allTasks
 
     fun insert(task: Task) = viewModelScope.launch {
+        Log.i("MyTag", "data inserted")
         repository.insert(task)
     }
 }
